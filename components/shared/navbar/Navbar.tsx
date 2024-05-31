c70d8bab38831cd0f9354037e7469bbb74f493b4
SOHAG-HOSSAIN\sohag <sohaghossainice@gmail.com>
2024-05-31 10:01:01 +0600

    webhook user creation

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           src="/assets/images/site-logo.svg"
           width={23}
           height={23}
-          alt="DevFlow"
+          alt="ICE"
         />
 
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
