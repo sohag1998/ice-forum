747a80a32eb36416c63f66585bd354c70905fd6e
SOHAG-HOSSAIN\sohag <sohaghossainice@gmail.com>
2024-06-03 17:02:11 +0600

    #

--- components/shared/QuestionTab.tsx
+++ components/shared/QuestionTab.tsx
@@ -6,7 +6,7 @@ import Pagination from "./Pagination";
 
 interface Props extends SearchParamsProps {
   userId: string;
-  clerkId?: string | "";
+  clerkId?: string;
 }
 
 const QuestionTab = async ({ searchParams, userId, clerkId }: Props) => {
